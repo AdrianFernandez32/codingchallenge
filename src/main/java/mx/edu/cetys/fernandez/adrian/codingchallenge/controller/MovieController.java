c6754a14c290104880c9cb21d547172056f13f48
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2024-10-15 14:36:17 -0700

    added movie controller

---
+++ src/main/java/mx/edu/cetys/fernandez/adrian/codingchallenge/controller/MovieController.java
@@ -0,0 +1,22 @@
+package mx.edu.cetys.fernandez.adrian.codingchallenge.controller;
+
+import mx.edu.cetys.fernandez.adrian.codingchallenge.service.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/search/{title}")
+    public CompletableFuture<String> searchMovies(@PathVariable String title) throws Exception {
+        return movieService.searchMovies(title);
+    }
+}
