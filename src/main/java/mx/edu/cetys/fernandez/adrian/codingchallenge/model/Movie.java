fa4c82dc9730fa9baec74389aaad6bfb733401f9
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2024-10-15 14:20:24 -0700

    fixed bugs and added movie model

---
+++ src/main/java/mx/edu/cetys/fernandez/adrian/codingchallenge/model/Movie.java
@@ -0,0 +1,28 @@
+package mx.edu.cetys.fernandez.adrian.codingchallenge.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    public Movie() {}
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
