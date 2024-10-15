c3b71cfa05d86f39c3f65aafbfea72bc06bf51cf
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2024-10-14 19:31:07 -0700

    started over

--- src/main/java/mx/edu/cetys/fernandez/adrian/coding_challenge/MovieService.java
+++ src/main/java/mx/edu/cetys/fernandez/adrian/coding_challenge/MovieService.java
@@ -1,5 +1,8 @@
 package mx.edu.cetys.fernandez.adrian.coding_challenge;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,29 +13,5 @@ import java.util.concurrent.Future;
 
 @Service
 public class MovieService {
-    private final RestTemplate restTemplate = new RestTemplate();
     private final String API_KEY = "88295e9002394ab72b2199f25ff94ce7";
-
-    public String getMovieDetails(String movieId) {
-        String url = String.format("https://api.themoviedb.org/3/movie/%s?api_key=%s", movieId, API_KEY);
-        return restTemplate.getForObject(url, String.class);
-    }
-
-    public List<String> getMultipleMoviesDetails(List<String> movieIds) {
-        List<String> moviesDetails = new ArrayList<>();
-
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            List<Future<String>> futures = movieIds.stream()
-                    .map(id -> executor.submit(() -> getMovieDetails(id)))
-                    .toList();
-
-            for (Future<String> future : futures) {
-                moviesDetails.add(future.get());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return moviesDetails;
-    }
 }
