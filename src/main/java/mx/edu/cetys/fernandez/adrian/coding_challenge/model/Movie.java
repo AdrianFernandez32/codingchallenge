af302b50c5eb72bd1cd0ed378f0f7ededb4c7d78
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2024-10-15 13:54:05 -0700

    added model?

--- src/main/java/mx/edu/cetys/fernandez/adrian/coding_challenge/model/Movie.java
+++ src/main/java/mx/edu/cetys/fernandez/adrian/coding_challenge/model/Movie.java
@@ -1,2 +1,29 @@
-package mx.edu.cetys.fernandez.adrian.coding_challenge.model;public class Movie {
+package mx.edu.cetys.fernandez.adrian.coding_challenge.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    public Movie() {}
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
